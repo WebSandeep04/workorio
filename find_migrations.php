0ecc310b6a5d9654e5ff2c8d89c7e794ad27f549
Sandeep Kumar <hey.sandeep.04@gmail.com>
2026-09-03 12:52:21 +0530

    cleaned the code base

--- find_migrations.php
+++
@@ -1,8 +0,0 @@
-<?php
-$files = glob('database/migrations/*.php');
-foreach ($files as $file) {
-    $content = file_get_contents($file);
-    if (!str_contains($content, 'getConnection()->getName()')) {
-        echo basename($file) . PHP_EOL;
-    }
-}
