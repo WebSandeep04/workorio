76a60480bd0fe01a1fc89b1d5dd12f8c9bbef940
Sandeep Kumar <hey.sandeep.04@gmail.com>
2026-09-14 13:06:44 +0530

    shifts added in side bar

--- config/menu.php
+++ config/menu.php
@@ -350,6 +350,7 @@ return [
                     'icon' => 'bi bi-calendar-check',
                     'roles' => ['admin'],
                     'items' => [
+                        ['route' => 'shifts.index', 'title' => 'Shifts', 'icon' => 'bi bi-clock', 'permission' => 'setup.shifts', 'feature_flag' => 'is_attendance_setup_enabled'],
                         ['route' => 'holiday', 'title' => 'Holidays', 'icon' => 'bi bi-calendar2-event', 'permission' => 'setup.holidays', 'feature_flag' => 'is_attendance_setup_enabled'],
                         ['route' => 'late-reasons.index', 'title' => 'Late Reasons', 'icon' => 'bi bi-clock-history', 'permission' => 'setup.late_reasons', 'feature_flag' => 'is_master_setup_enabled'],
                         ['route' => 'calendar-missed-reasons.index', 'title' => 'Missed Reason', 'icon' => 'bi bi-calendar-x', 'feature_flag' => 'is_calendar_setup_enabled', 'permission' => 'setup.calendar_missed_reasons'],
